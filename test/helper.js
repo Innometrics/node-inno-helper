45ddf684eca1aca60bc93c6e2197b3a588dbf824
roman <roman.ziva@gmail.com>
2015-07-17 16:42:33 +0300

    refs #8078

--- test/helper.js
+++ test/helper.js
@@ -78,28 +78,53 @@ describe.only('Inno Helper', function () {
             helper = createHelper(config);
         });
 
-        it('should properly get settings', function () {
-            assert.strictEqual(helper.getBucket(), config.bucketName);
-            assert.strictEqual(helper.getCollectApp(), config.appName);
-            assert.strictEqual(helper.getAppKey(), config.appKey);
-            assert.strictEqual(helper.getApiHost(), config.apiUrl);
-            assert.strictEqual(helper.getCompany(), config.groupId);
+        describe('settings', function () {
+
+            it('should properly get settings', function () {
+                assert.strictEqual(helper.getBucket(), config.bucketName);
+                assert.strictEqual(helper.getCollectApp(), config.appName);
+                assert.strictEqual(helper.getAppKey(), config.appKey);
+                assert.strictEqual(helper.getApiHost(), config.apiUrl);
+                assert.strictEqual(helper.getCompany(), config.groupId);
+            });
+
         });
 
-        it('should return ProfileUrl', function () {
-            assert.equal(helper.getProfileUrl('some-profile'), 'apiUrl/v1/companies/4/buckets/bucketName/profiles/some-profile?app_key=appKey');
+        describe('url generation', function () {
+
+            it('should return ProfileUrl', function () {
+                assert.equal(helper.getProfileUrl('some-profile'), 'apiUrl/v1/companies/4/buckets/bucketName/profiles/some-profile?app_key=appKey');
+            });
+
+            it('should return AppSettingsUrl', function () {
+                assert.equal(helper.getAppSettingsUrl(), 'apiUrl/v1/companies/4/buckets/bucketName/apps/appName/custom?app_key=appKey');
+            });
+
+            it('should return SegmentsUrl', function () {
+                assert.equal(helper.getSegmentsUrl(), 'apiUrl/v1/companies/4/buckets/bucketName/segments?app_key=appKey');
+            });
+
+            it('should return SegmentEvaluationUrl', function () {
+                assert.equal(helper.getSegmentEvaluationUrl({param1: 'value1'}), 'apiUrl/v1/companies/4/buckets/bucketName/segment-evaluation?app_key=appKey&param1=value1');
+            });
+
         });
 
-        it('should return AppSettingsUrl', function () {
-            assert.equal(helper.getAppSettingsUrl(), 'apiUrl/v1/companies/4/buckets/bucketName/apps/appName/custom?app_key=appKey');
-        });
+        describe('profile', function () {
 
-        it('should return SegmentsUrl', function () {
-            assert.equal(helper.getSegmentsUrl(), 'apiUrl/v1/companies/4/buckets/bucketName/segments?app_key=appKey');
-        });
+            it('should properly create new profile', function () {
+                var id = 'profile-id',
+                    profile = helper.createProfile('profile-id');
 
-        it('should return SegmentEvaluationUrl', function () {
-            assert.equal(helper.getSegmentEvaluationUrl({param1: 'value1'}), 'apiUrl/v1/companies/4/buckets/bucketName/segment-evaluation?app_key=appKey&param1=value1');
+                assert.equal(profile.getId(), id);
+                assert.deepEqual(profile.getSessions(), []);
+                assert.deepEqual(profile.getAttributes(), []);
+            });
+
+            // it('should properly create profile from Profile Stream', function () {
+
+            // });
+
         });
 
     });
